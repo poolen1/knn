db4dca7e70015b9307059f117c3fc1c29410f0b4
poolen108 <poolen108@gmail.com>
2021-02-25 11:47:19 -0800

    knn dummy methods

---
+++ knn.py
@@ -0,0 +1,16 @@
+
+class KNN:
+    def __init__(self, K):
+        self.K = K
+
+    def get_distance(self):
+        pass
+
+    def get_NN(self):
+        pass
+
+    def gather_categories(self):
+        pass
+
+    def vote(self):
+        pass
