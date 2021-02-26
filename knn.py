af6a1adfd80b855e092862837539094a5097349f
poolen108 <poolen108@gmail.com>
2021-02-25 20:34:40 -0800

    get_neighbors

--- knn.py
+++ knn.py
@@ -1,16 +1,37 @@
 
 class KNN:
-    def __init__(self, K):
-        self.K = K
+    def __init__(self, training, test, k):
+        self.K = k
+        self.training_data = training
+        self.test_data = test
+        self.p = 2
 
-    def get_distance(self):
-        pass
+    def minkowski_distance(self, query):
+        diffs = []  # List of (training_item, Minkowski_Val)
+        the_sum = 0  # Sum of the difference of feature dimensions
+        x = 0  # Difference btw query and example item
+        mink_value = 0
+        for item in self.training_data:
+            for i in range(0, len(item)):
+                x = abs(query[i] - item[i]) ** self.p
+                the_sum += x
+            mink_value = the_sum ** (1/self.p)
+            diffs.append(item, mink_value)
+        return diffs
 
-    def get_NN(self):
-        pass
+    def get_neighbors(self, query):
+        nn = [] * self.k
+        diffs = self.minkowski_distance(query)
+        diffs.sort(reverse=True)
+        nn = diffs[0:self.k]
+        return nn
 
     def gather_categories(self):
         pass
 
     def vote(self):
+        pass
+
+    def classify_data(self, data):
+        knn = self.get_neighbors()
         pass
--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import pandas as pd
+from sklearn.decomposition import PCA
+from knn import KNN
+import matplotlib.pyplot as plt
 import numpy
 
-# f = open("data/MNIST_training.csv", "r")
+# Read in data from files
 training_set = pd.read_csv("data/MNIST_training.csv")
 test_set = pd.read_csv("data/MNIST_test.csv")
 
-print(training_set)
+
+#  Function to project multi-D data onto 2D plane
+def project_data(data):
+    X = data.drop('label', axis=1)
+    pca = PCA(n_components=2)
+    pca.fit(X)
+    PCAX = pca.transform(X)
+    return PCAX
+
+
+# Model training
+# =================================
+# Project data onto 2 dimensions
+y = training_set.iloc[:, 0]
+X = training_set.drop('label', axis=1)
+training_data = (y, X)
+# training_PCA = project_data(training_set)
+# training_data = (y, training_PCA)
+
+y = test_set.iloc[:, 0]
+X = training_set.drop('label', axis=1)
+test_data = (y, X)
+# test_PCA = project_data(test_set)
+# test_data = (y, test_PCA)
+
+classifier = KNN(training_data, test_data, 1)
