86fa18e1475fef7b417f340040a8700a3e2425fb
poolen108 <poolen108@gmail.com>
2021-03-02 18:45:52 -0800

    doc

--- knn.py
+++ knn.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 
+# =================================
 class KNN:
     def __init__(self, training, test, k, p):
-        self.k = k
+        self.k = k  # Number of NN chosen
         self.training_data = training
         self.test_data = test
-        self.p = p
+        self.p = p  # Minkowski distance parameter
 
+    # Calculate distances of all training points
+    # from query data point
+    # =================================
     def minkowski_distance(self, query):
         diffs = []  # List of (training_item, Minkowski_Val)
         training_labels = self.training_data.iloc[:, 0]
@@ -15,16 +19,24 @@ class KNN:
         for i in range(len(query)):
             the_sum = sum(abs(query - training_items[i]) ** self.p)
             mink_value = the_sum ** (1/float(self.p))
+            # Distance tuple contains distance value
+            # and the label of the associated training data point
             diff = (mink_value, training_labels[i])
             diffs.append(diff)
         return diffs
 
+    # Calculate distances, sort and slice
+    # distances array to return k (distance, label) tuples
+    # =================================
     def get_neighbors(self, query):
         diffs = self.minkowski_distance(query)
         diffs.sort(key=lambda x: x[0])
         nn = diffs[:self.k]
         return nn
 
+    # Instantiate ballot with label tuple (label, number_of_votes)
+    # Number of votes initialized with 0
+    # =================================
     @staticmethod
     def gather_categories():
         labels = []
@@ -33,25 +45,37 @@ class KNN:
             labels.append(label)
         return labels
 
+    # Simple majority of query's KNN's label values
+    # creates query's predicted label
+    # =================================
     def vote(self, knn):
         max_val = 0
         category = 0
-        # Labels array: contains set of categories, and value that represents
+        # Labels array: contains set of categories,
+        # and value that represents
         # number of NN with the same category
         labels = np.array(self.gather_categories())
 
-        # Vote: iterate NN and increment their categories in the labels array
+        # Vote: iterate NN and increment their
+        # categories in the labels array
         for item in knn:
             for label in labels:
                 if item[1] == label[0]:
                     label[1] += 1
 
+        # Iterate through labels after
+        # polls have closed to find
+        # chosen prediction
         for item in labels:
             if item[1] > max_val:
                 max_val = item[1]
                 category = item[0]
+
         return category
 
+    # Iterate test data rows to get
+    # label predictions for each item
+    # =================================
     def classify_data(self):
         categories = []
         for item in self.test_data[1]:
@@ -59,6 +83,10 @@ class KNN:
             categories.append(self.vote(knn))
         return categories
 
+    # Compare array of predicted labels
+    # with ground truth labels,
+    # output percent accuracy
+    # =================================
     @staticmethod
     def evaluate(ground_truth, predictions):
         total_correct = 0
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from knn import KNN
 from datetime import datetime
 
 # Read in data from files
+# =================================
 training_set = pd.read_csv("data/MNIST_training.csv")
 test_set = pd.read_csv("data/MNIST_test.csv")
 
-# Model training
+# Data Processing
 # =================================
 
 y_list = test_set.iloc[:, 0]
@@ -18,15 +19,24 @@ X = np.array(X_list)
 test_data = (y, X)
 ground_truth = y
 
+# KNN Prediction
+# =================================
+
 start = datetime.now()
 classifier = KNN(training_set, test_data, 3, 2)
 predictions = classifier.classify_data()
+
+# Prediction Accuracy
+# =================================
+
 prediction_percent = classifier.evaluate(ground_truth, predictions)
 print("Accuracy: ", prediction_percent, "%")
 print("Runtime: ", datetime.now() - start)
 
 # best: k=3, p=2 (Euclidean distance), 82%
+# Meta-parameter Analysis
 # Loop through k in range (1,10) and p in range (1.0, 2.0)
+# =================================
 """
 metadata = []
 for i in range(1, 11):
